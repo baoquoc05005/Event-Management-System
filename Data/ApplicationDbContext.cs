5b4650ee7aa2a8ea6c8a7739497e1371f50a7696
baoquoc05005 <baoquoc05005@gmail.com>
2026-04-12 18:40:43 -0400

    Add Module 3: Event Registration System with capacity checking and waitlist

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -10,6 +10,7 @@ namespace EventManagementSystem.Data
         public DbSet<User> Users { get; set; }
         public DbSet<Role> Roles { get; set; }
         public DbSet<Event> Events { get; set; }
+        public DbSet<Registration> Registrations { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
